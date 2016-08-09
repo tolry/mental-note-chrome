9e71f70581e771be3983015b0877b0c95e2bc560
Robin Willig <robin@dragonito.net>
2016-08-09 21:27:47 +0200

    Added newTab logic

--- src/background.js
+++ src/background.js
@@ -1,10 +1,8 @@
 var options = {};
 
 function restore_options() {
-    chrome.storage.sync.get({'baseUrl': '', 'newTab': ''}, function (items) {
+    chrome.storage.sync.get({'baseUrl': '', 'newTab': false}, function (items) {
         options = items;
-        document.getElementById('base-url').value = items.baseUrl;
-        document.getElementById('newtab').checked = items.newTab;
     });
 };
 
@@ -12,5 +10,14 @@ restore_options();
 
 chrome.browserAction.onClicked.addListener(function (tab) {
     var url = options.baseUrl + "quick-add?url=" + encodeURIComponent(tab.url);
-    chrome.tabs.create({url: url});
+
+    if (options.newTab === true) {
+        chrome.tabs.create({url: url});
+    } else {
+        chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
+            chrome.tabs.update(tab.id, {url: url});
+        });
+    }
+
 });
+
